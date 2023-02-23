ff5e7cebc16ac88f7b66daf9ee89efb74c9b8325
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-02-22 16:03:36 -0800

    Add test for fern request spec/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      namespace :users do
+        get '/greenhouse', to: "ferns#index"
+      end
+    end
+  end
 end
--- spec/requests/api/v1/fern_request_spec.rb
+++ spec/requests/api/v1/fern_request_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "ferns API endpoints" do
-  it 'sends a list of all'
+  it 'sends a list of all a users ferns' do
+    user = create(:user)
+    fern1 = create(:fern, user_id: user.id)
+    fern2 = create(:fern, user_id: user.id)
+    fern3 = create(:fern, user_id: user.id)
+
+    get '/api/v1/greenhouse'
+    # google_auth_params = {
+    #   name: "Tony Pepperoni",
+    #   email: "thebigpepperoni@gmail.com",
+    #   google_id: "iosdfh454"
+    # }
+  end
 
 
 end
