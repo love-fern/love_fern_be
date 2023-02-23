80d994266feda323b40c3cb15abaf68a1b635d6d
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-02-23 10:33:24 -0800

    Add controller functionality to shelf upd, des

--- app/controllers/api/v1/users/shelves_controller.rb
+++ app/controllers/api/v1/users/shelves_controller.rb
@@ -4,7 +4,19 @@ class Api::V1::Users::ShelvesController < ApplicationController
     render json: ShelfSerializer.new(new_shelf)
   end
 
+  def update
+    render json: ShelfSerializer.new(Shelf.update(update_params))
+  end
+
+  def destroy
+    Shelf.find(params[:id]).destroy
+  end
+
   private
+
+  def update_params
+    params.require(:shelf).permit(:name)
+  end
 
   def shelf_params
     params.require(:shelf).permit(:name, :user_id)
