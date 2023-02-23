64d0727f9f677e5e0cd759ce5b4b9fad8d5a57db
Brady Rohrig <brady.rohrig@gmail.com>
2023-02-22 20:13:58 -0800

    test: wrote test for user update method

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -17,6 +17,22 @@ RSpec.describe "user API requests" do
     expect(created_user.google_id).to eq("23948sdkss")
   end
 
+  it 'can modify a users name' do
+    user = create(:user)
+    id = user.id
+    expect(user.name).to_not eq("this is my new name")
+
+    name_change_params = { name: "this is my new name" }
+    headers = { 'CONTENT_TYPE' => 'application/json' }
+    patch "/api/v1/user/#{id}", headers: headers, params: JSON.generate(user: name_change_params)
+
+    updated_user = User.find_by(id: id)
+    expect(response).to be_successful
+    expect(updated_user.name).to eq("this is my new name")
+    expect(updated_user.email).to eq(user.email)
+    expect(updated_user.google_id).to eq(user.google_id)
+  end
+
 
 
 end
