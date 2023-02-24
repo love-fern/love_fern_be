bc326c7efe1f4c5d841978b96889103a0a7fab37
Brady Rohrig <brady.rohrig@gmail.com>
2023-02-23 19:08:28 -0800

    test: wrote test for sentiment_service happy path

--- spec/services/google_sentiment_service_spec.rb
+++ spec/services/google_sentiment_service_spec.rb
@@ -1,6 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe "google sentiment service" do
+  it 'returns a response analyzing an input text' do
+    query = {
+              "document": {
+                "content": "Hello. I am a muffin. Eat me.",
+                "language": "",
+                "type": "PLAIN_TEXT"
+                          },
+              "encodingType": "UTF8"
+            }
+    sentiment_response = GoogleSentimentService.get_sentiment(query)
+    expect(sentiment_response).to eq({
+                        documentSentiment: {
+                          magnitude: 0.5,
+                          score: -0.1
+                        },
+                        language: "en",
+                        sentences: [
+                          {
+                            text: {
+                              content: "Hello.",
+                              beginOffset: 0
+                            },
+                            sentiment: {
+                              magnitude: 0.1,
+                              score: 0.1
+                            }
+                          },
+                          {
+                            text: {
+                              content: "I am a muffin.",
+                              beginOffset: 7
+                            },
+                            sentiment: {
+                              magnitude: 0.3,
+                              score: -0.3
+                            }
+                          },
+                          {
+                            text: {
+                              content: "Eat me.",
+                              beginOffset: 22
+                            },
+                            sentiment: {
+                              magnitude: 0.1,
+                              score: -0.1
+                            }
+                          }
+                        ]
+                      })
+  end
 
 
 end
