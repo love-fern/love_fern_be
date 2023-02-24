7999fd54192e8f9ab19090ad740bdf9a2f5a9708
Drew Layton <dlayton66@gmail.com>
2023-02-24 12:43:25 -0700

    test: Start reworking get all shelves/ferns test

--- spec/requests/api/v1/shelf_request_spec.rb
+++ spec/requests/api/v1/shelf_request_spec.rb
@@ -52,20 +52,57 @@ RSpec.describe "shelves API endpoints" do
     expect{ Shelf.find(shelf1.id) }.to raise_error(ActiveRecord::RecordNotFound)
   end
 
-  it 'can get all the shelves associated with a user' do
+  it 'can get all the shelves and ferns associated with a user' do
     user = create(:user)
     shelf = create(:shelf, user_id: user.id)
-    fern = create(:fern, shelf_id: shelf.id)
-    fern2 = create(:fern, shelf_id: shelf.id)
-    fern3 = create(:fern, shelf_id: shelf.id)
-    fern4 = create(:fern, shelf_id: shelf.id)
+    ferns = create_list(:fern, 4, shelf_id: shelf.id)
 
     get api_v1_user_shelf_ferns_path(user.id, shelf.id)
 
     expect(response).to be_successful
-    ferns_response = JSON.parse(response.body, symbolize_names: true)[:data]
 
-    expect(ferns_response[0][:attributes][:name]).to eq(shelf.name)
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+    shelves_response = parsed_response[:data]
+    ferns_response = parsed_response[:included]
+
+    expected_shelf_names = ['Friends', 'Family', 'Romantic', 'Business', shelf.name]
+
+    shelves_response.each_with_index do |shelf, i|
+      expect(shelf).to have_key(:id)
+      expect(shelf[:id]).to be_a(String)
+      
+      expect(shelf).to have_key(:type)
+      expect(shelf[:type]).to eq("shelf")
+      
+      expect(shelf).to have_key(:attributes)
+      expect(shelf[:attributes]).to have_key(:name)
+      expect(shelf[:attributes][:name]).to eq(expected_shelf_names[i])
+
+      expect(shelf).to have_key(:relationships)
+
+      expect(shelf[:relationships]).to have_key(:user)
+      expect(shelf[:relationships][:user][:data]).to be_a(Hash)
+      expect(shelf[:relationships][:user][:data][:id]).to eq(user.id.to_s)
+      expect(shelf[:relationships][:user][:data][:type]).to eq("user")
+
+      expect(shelf[:relationships]).to have_key(:ferns)
+
+      related_ferns = shelf[:relationships][:ferns][:data]
+
+      expect(related_ferns).to be_an(Array)
+
+      if i < 4
+        expect(related_ferns).to be_empty # default shelves have no ferns
+      elsif i == 4
+        related_ferns.each_with_index do |fern, i|
+          expect(fern[:id]).to eq(ferns[i].id.to_s)
+          expect(fern[:type]).to eq("fern")
+        end
+      end
+    end
+
+    binding.pry
+
     expect(ferns_response[0][:attributes][:ferns][0]).to be_a(Hash)
     expect(ferns_response[0][:attributes][:ferns][0][:name]).to eq(fern.name)
     expect(ferns_response[0][:attributes][:ferns][0][:health]).to eq(fern.health)
