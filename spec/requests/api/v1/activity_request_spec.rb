b8d4883cf7b2f9863d3b38809ed413be7313e44f
Brady Rohrig <brady.rohrig@gmail.com>
2023-02-24 09:48:41 -0800

    fix: added vcr to activities api testing

--- spec/requests/api/v1/activity_request_spec.rb
+++ spec/requests/api/v1/activity_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'activity API endpoint' do
   context 'requesting a random activity' do
-   it 'sends a random activity using Bored API' do
+   it 'sends a random activity using Bored API', :vcr do
       get api_v1_activities_path
 
       expect(response).to be_successful
