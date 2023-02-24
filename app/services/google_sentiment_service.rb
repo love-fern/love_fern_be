a94d7b67aa5cef5a9b59f67ca372fa8688c1ee38
Brady Rohrig <brady.rohrig@gmail.com>
2023-02-23 21:28:47 -0800

    feat: implemented service call to google sentiment API and implemented VCR

---
+++ app/services/google_sentiment_service.rb
@@ -0,0 +1,29 @@
+class GoogleSentimentService
+  def self.get_sentiment(message)
+    # binding.pry
+    request = conn.post("/v1/documents:analyzeSentiment", self.google_query_format(message))
+     
+    JSON.parse(request.body, symbolize_names: true)
+  end
+
+  private
+
+  def self.conn
+    Faraday.new(url: "https://language.googleapis.com") do |f|
+      f.headers["content-type"] = 'text/plain'
+      f.params["key"] = ENV["GOOGLE_API_KEY"]
+    end
+  end
+
+  def self.google_query_format(input)
+    {
+      "document": {
+        "content": input,
+        "language": "",
+        "type": "PLAIN_TEXT"
+      },
+      "encodingType": "UTF8"
+    }.to_json
+  end
+
+end
