b7a662385971c1a4fe76fdc58c5676ffe8d8f5bf
Drew Layton <dlayton66@gmail.com>
2023-02-28 16:53:05 -0700

    fix: Minor fixes to ferns_controller

--- app/controllers/api/v1/users/ferns_controller.rb
+++ app/controllers/api/v1/users/ferns_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Users::FernsController < ApplicationController
   def index
-    user = User.find_by_id(params[:user_id])
+    user = User.find_by(google_id: params[:user_id])
     render json: FernSerializer.new(user.ferns)
   end
 
@@ -14,7 +14,7 @@ class Api::V1::Users::FernsController < ApplicationController
     shelf = user.shelves.find_by(name: params["shelf"])
     new_fern = shelf.ferns.new(fern_params)
     if new_fern.save
-      render json: FernSerializer.new(new_fern)
+      render json: FernSerializer.new(new_fern), status: 201
     else
       render json: ::ErrorsController.bad_request, status: 404
     end
