63c60c91790ee6b4164fe612a8e5a38bbaa957e5
Drew Layton <dlayton66@gmail.com>
2023-03-19 15:14:22 -0600

    feat: Add health history stats to serializer

--- app/serializers/fern_serializer.rb
+++ app/serializers/fern_serializer.rb
@@ -1,6 +1,12 @@
 class FernSerializer
   include JSONAPI::Serializer
   attributes :name, :health, :preferred_contact_method
+  attribute :stats, if: Proc.new { |fern, params|
+    params[:stats]
+  } do |fern|
+    fern.health_history
+  end
+
   belongs_to :shelf
   has_one :user
   has_many :interactions do |fern|
