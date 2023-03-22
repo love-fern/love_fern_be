1ae0ecf74d57461c54fceb7074c5257361aa071d
Drew Layton <dlayton66@gmail.com>
2023-03-22 08:53:25 -0600

    fix: Add user to include in stats index call

--- app/controllers/api/v1/users/ferns/stats_controller.rb
+++ app/controllers/api/v1/users/ferns/stats_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Users::Ferns::StatsController < ApplicationController
   def index
     fern = Fern.find(params[:fern_id])
-    options = { include: [:interactions],
+    options = { include: %i[interactions user],
                 params: {stats: true} }
     render json: FernSerializer.new(fern, options)
   end
