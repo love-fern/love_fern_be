f7290c4eb2bdb1061c8142be473a787c0810807c
Drew Layton <dlayton66@gmail.com>
2023-03-22 08:54:38 -0600

    fix: Fern serializer passing three vs all interactions

--- app/serializers/fern_serializer.rb
+++ app/serializers/fern_serializer.rb
@@ -2,10 +2,13 @@ class FernSerializer
   include JSONAPI::Serializer
   attributes :name, :health, :preferred_contact_method
   
-  has_many :interactions do |fern|
-    fern.interactions.order(created_at: :desc).first(3)
-  end
-  has_many :interactions, if: Proc.new { |fern, params| params[:stats] }
   belongs_to :shelf
   has_one :user
+  has_many :interactions do |fern, params|
+    if params[:stats]
+      fern.interactions.order(created_at: :desc)
+    else
+      fern.interactions.order(created_at: :desc).first(3)
+    end
+  end
 end
